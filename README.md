82430d99141c0e5c62aae95d1a760366e3dae3ff
jiaohongtao <jiaohongtao@beyondcent.com>
2021-02-24 10:23:22 +0800

    更新文件读取功能

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 测试spring和html结合，个人研究
 
 # 已有功能请访问页面
-http://localhost:9090/hi/function/description
+[已有功能](http://localhost:9090/hi/func)
 
 # 参考文献
 - [SpringBoot + validation 接口参数校验](https://blog.csdn.net/u014553029/article/details/109192520)
--- src/main/java/com/hong/controller/FileReadController.java
+++ src/main/java/com/hong/controller/FileReadController.java
@@ -1,6 +1,8 @@
 package com.hong.controller;
 
+import cn.hutool.core.util.IdcardUtil;
 import com.hong.bean.Result;
+import com.hong.util.common.IDCardUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
@@ -16,6 +18,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 文件操作
@@ -44,5 +50,39 @@ public class FileReadController {
             e.printStackTrace();
         }
         return Result.success(stringBuilder.toString());
+    }
+
+    @PostMapping("/idCard/analyze")
+    @ApiOperation(value = "身份证号地址识别")
+    public Result idCardAnalyze(@RequestParam("fileName") MultipartFile file) {
+        StringBuilder stringBuilder = new StringBuilder();
+        List<Map<String, String>> personMsgList = new ArrayList<>();
+        try {
+            InputStreamReader reader = new InputStreamReader(file.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String string;
+            while ((string = bufferedReader.readLine()) != null) {
+                if (StringUtils.isNotBlank(string)) {
+                    String[] nameIdCard = string.split(" ");
+                    String name = nameIdCard[0];
+                    String idCard = nameIdCard[1];
+
+                    String city = IDCardUtil.getCity(idCard);
+                    stringBuilder.append(name).append("\t").append(city).append("\t");
+                    stringBuilder.append(idCard).append("\n");
+
+                    // 添加到list中
+                    Map<String, String> person = new HashMap<>();
+                    person.put("name", name);
+                    person.put("city", city);
+                    person.put("idCard", idCard);
+                    personMsgList.add(person);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // return Result.success(stringBuilder.toString());
+        return Result.success(personMsgList);
     }
 }

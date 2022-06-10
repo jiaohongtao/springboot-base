33d173116536fa2a3b2a7498de2d032aea023943
jiaohongtao <hongtao.jiao@seaboxdata.com>
2022-06-10 14:32:10 +0800

    常量类

---
+++ src/main/java/com/hong/bean/Constant.java
@@ -0,0 +1,11 @@
+package com.hong.bean;
+
+/**
+ * @author jiaohongtao
+ * @version 1.0.0
+ * @since 2022/06/10
+ */
+public class Constant {
+
+    public static final String CONCAT_TO_SPLIT = "###&&&";
+}

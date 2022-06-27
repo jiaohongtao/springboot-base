27fa63159c13f872b99b259c6f6f73fc538eafca
jiaohongtao <hongtao.jiao@seaboxdata.com>
2022-06-27 17:53:19 +0800

    spriboot + cas

--- src/main/java/com/hong/Application.java
+++ src/main/java/com/hong/Application.java
@@ -1,11 +1,13 @@
 package com.hong;
 
+import net.unicon.cas.client.configuration.EnableCasClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCasClient
 public class Application {
 
     public static void main(String[] args) {

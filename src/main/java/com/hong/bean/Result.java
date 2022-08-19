f5b0e852e3dde742ff80ff69d65532349951b027
jiaohongtao <hongtao.jiao@seaboxdata.com>
2022-08-19 14:25:43 +0800

    全局异常处理

--- src/main/java/com/hong/bean/Result.java
+++ src/main/java/com/hong/bean/Result.java
@@ -1,6 +1,7 @@
 package com.hong.bean;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -14,6 +15,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class Result {
     private boolean success;
     private Object data;

ef0c9fdf3ea1eff4f05d8126425f383bd070e79e
jiaohongtao <hongtao.jiao@seaboxdata.com>
2023-07-10 15:43:32 +0800

    优化消息监听

--- src/main/java/com/hong/service_util/rabbit/DirectReceiver.java
+++ src/main/java/com/hong/service_util/rabbit/DirectReceiver.java
@@ -1,20 +1,22 @@
 package com.hong.service_util.rabbit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitHandler;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
+@Slf4j
 @Component
 public class DirectReceiver {
 
     @RabbitHandler
     @RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
     public void process(Map<String, Object> testMessage) {
-        System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
+        log.info("DirectReceiver消费者收到消息  : {}" + testMessage.toString());
         Object messageData = testMessage.get("messageData");
-        System.out.println(messageData);
+        log.info("messageData是：{}", messageData);
     }
 
 }

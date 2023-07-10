d1593e9935c014f4604d60daaa845b3cb9256cb5
jiaohongtao <hongtao.jiao@seaboxdata.com>
2023-07-10 15:32:29 +0800

    优化消息监听位置到方法上

--- src/main/java/com/hong/service_util/rabbit/DirectReceiver.java
+++ src/main/java/com/hong/service_util/rabbit/DirectReceiver.java
@@ -7,10 +7,10 @@ import org.springframework.stereotype.Component;
 import java.util.Map;
 
 @Component
-@RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
 public class DirectReceiver {
 
     @RabbitHandler
+    @RabbitListener(queues = "TestDirectQueue")//监听的队列名称 TestDirectQueue
     public void process(Map<String, Object> testMessage) {
         System.out.println("DirectReceiver消费者收到消息  : " + testMessage.toString());
         Object messageData = testMessage.get("messageData");
